279754d2718e0110bfff23c8e00e754396612e9a
serpiara <sergio.pizarroo@outlook.com>
2020-07-15 11:03:39 -0400

    Método request refactorizado, se envian por paramtros al método url y api key

--- nasa.rb
+++ nasa.rb
@@ -1,16 +1,26 @@
-def request(url_requested)
-        url = URI(url_requested)
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+def request(address,app_key)
+        require "uri"
+        require "net/http"
+        require 'json'
+        
+        url = URI"#{address}#{app_key}"
+       
+        https = Net::HTTP.new(url.host, url.port);
+        https.use_ssl = true
+        
         request = Net::HTTP::Get.new(url)
-        request['app_id'] = 'serpiara@gmail.com'
-        request['app_key'] = 'wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3'
-        response = http.request(request)
-        return JSON.parse(response.body)
+        
+        response = https.request(request)
+        body = JSON.parse response.read_body
+        
+        #pp body
+        
+        body.each do |photos_rover|
+                puts photos_rover['id'.to_i]
+                        photos_rover.each do |camera_rover|
+                                puts camera_rover['sol'.to_i]
+                        end
+        end  
 end
 
-
-
-def buid_web_page
-end
+request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&","api_key=wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3")
