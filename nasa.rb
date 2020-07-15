5ff24aae211b4b88fc7133cd6e0771b714db7fb6
serpiara <sergio.pizarroo@outlook.com>
2020-07-15 16:13:48 -0400

    Se definieron completamente los metodos request y buil_web_page, falta que las imagenes carguen en las etiquetas correctas

--- nasa.rb
+++ nasa.rb
@@ -1,26 +1,42 @@
 def request(address,app_key)
-        require "uri"
-        require "net/http"
-        require 'json'
-        
+    require "uri"
+    require "net/http"
+    require 'json'
         url = URI"#{address}#{app_key}"
-       
         https = Net::HTTP.new(url.host, url.port);
         https.use_ssl = true
-        
         request = Net::HTTP::Get.new(url)
-        
         response = https.request(request)
+#puts response.read_body.class
         body = JSON.parse response.read_body
-        
-        #pp body
-        
-        body.each do |photos_rover|
-                puts photos_rover['id'.to_i]
-                        photos_rover.each do |camera_rover|
-                                puts camera_rover['sol'.to_i]
-                        end
-        end  
+#Este es el hash
+#puts body.class  
+        return body
+end     
+
+
+def buid_web_page(body)
+#puts body
+#puts body['photos']
+    new_array = []
+    new_array = body['photos']
+
+    puts new_array[1]['img_src']
+    inicio = 0
+        new_array.each do |x|
+            puts "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+            identi = new_array[inicio]['id']
+            imagen = new_array[inicio]['img_src']
+puts identi
+puts imagen
+            inicio += 1                
+            puts "\n\t\t<ul>\n\t</body>\n</html>"
+        end
+
 end
 
-request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&","api_key=wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3")
+#Bloque del programa
+
+#request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&","api_key=wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3")
+buid_web_page(request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&","api_key=wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3"))
+
