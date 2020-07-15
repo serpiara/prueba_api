a76f52a8a1499a5924e4042ffa04aac0a6a39276
serpiara <sergio.pizarroo@outlook.com>
2020-07-15 07:23:18 -0400

    Se configura la llave secreta obtenida de la API de la Nasa

--- nasa.rb
+++ nasa.rb
@@ -4,8 +4,8 @@ def request(url_requested)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
         request = Net::HTTP::Get.new(url)
-        request['app_id'] = 'ID'
-        request['app_key'] = 'llave_secreta'
+        request['app_id'] = 'serpiara@gmail.com'
+        request['app_key'] = 'wqTZTANmx0kuzDXWqKHlCf39ggBaciwejQhSe7l3'
         response = http.request(request)
         return JSON.parse(response.body)
 end
